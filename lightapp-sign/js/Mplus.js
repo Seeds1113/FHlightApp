979afe516a3da4f7ff4beef4f737c7804c141c8e
pengle <382064873@qq.com>
2016-03-22 15:25:27 +0800

    新增

---
+++ lightapp-sign/js/Mplus.js
@@ -0,0 +1,19 @@
+var Mplus = {
+	//3.7.3 获取当前地理位置接口
+	getLocation : function(success,fail){
+		mplus.getLocation({
+		    success: function (res) {
+		    	alert(1)
+		    	success && success(res);
+		        // var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+		        // var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+		        // var speed = res.speed; // 速度，以米/每秒计
+		        // var accuracy = res.accuracy; // 位置精度
+			},
+			fail: function (res) {
+				alert(2)
+			    fail && fail(res);
+			}
+		});
+	}
+}
