4fabdb3c4bd625a7fff02af426ca193c16b66540
pengle <382064873@qq.com>
2016-03-22 16:39:16 +0800

    更新token

--- lightapp-sign/js/ajaxModel.js
+++ lightapp-sign/js/ajaxModel.js
@@ -11,7 +11,7 @@
 			reqSource:"client"*/
 		}
 	});
-	var sessionId = escape("mobileark#6e874498-e663-4c5d-a993-58dfa42a2484");//mobileark#5f0390da-164a-420e-bf87-5fcd4a0eef82
+	var sessionId = escape("mobileark#ea688b26-5d6e-45cf-912e-678b7ad2a1b5");//mobileark#5f0390da-164a-420e-bf87-5fcd4a0eef82
 	// sessionId = window.localStorage.getItem("sessionId");
 	/*document.addEventListener("plusready",function(){
 		sessionId = window.localStorage.getItem("sessionId");
